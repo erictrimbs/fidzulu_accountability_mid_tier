5d604583029c78b77ccc6a56346e5b3da819979d
Dennisn1010 <90853201+Dennisn1010@users.noreply.github.com>
2021-09-16 14:59:33 -0400

    updated tests to have mock data

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,23 +1,31 @@
 const request = require("request");
 
-const base_url = 'http://localhost:3021/';
+const base_url = 'http://localhost:3021/classA/';
+
+const mockResponseBikeDurham = { 'request is': 'valid', 'service name': 'bike', 'location': 'durham' };
+const mockResponseFoodDurham = { 'request is': 'valid', 'service name': 'food', 'location': 'durham' };
+const mockResponseToysDurham = { 'request is': 'valid', 'service name': 'toys', 'location': 'durham' };
+
+const mockResponseBikeRaleigh = { 'request is': 'valid', 'service name': 'bike', 'location': 'raleigh' };
+const mockResponseFoodRaleigh = { 'request is': 'valid', 'service name': 'food', 'location': 'raleigh' };
+const mockResponseToysRaleigh = { 'request is': 'valid', 'service name': 'toys', 'location': 'raleigh' };
 
 describe("Server should", () => {
-    describe("GET /", () => {
-        it("and return <b>My</b> first express http server", (done) => {
-            request.get(base_url, (error, response, body) => {
-                expect(body).toBeTruthy();
-                expect(body).toContain("<b>My</b> first express http server");
-                done();
-            });
-        });
-    });
+    // describe("GET /", () => {
+    //     it("and return <b>My</b> first express http server", (done) => {
+    //         request.get(base_url, (error, response, body) => {
+    //             expect(body).toBeTruthy();
+    //             expect(body).toContain("<b>My</b> first express http server");
+    //             done();
+    //         });
+    //     });
+    // });
 
     describe("GET /:servicename/all/:location", () => {
         it("and return all bikes in durham", (done) => {
             request.get(base_url + 'bike/all/durham', (error, response, body) => {
                 expect(body).toBeTruthy();
-                expect(body).toContain("");
+                expect(body).toContain(JSON.stringify(mockResponseBikeDurham));
                 done();
             });
         });
@@ -25,7 +33,7 @@ describe("Server should", () => {
         it("and return all foods in durham", (done) => {
             request.get(base_url + 'food/all/durham', (error, response, body) => {
                 expect(body).toBeTruthy();
-                expect(body).toContain("");
+                expect(body).toContain(JSON.stringify(mockResponseFoodDurham));
                 done();
             });
         });
@@ -33,7 +41,7 @@ describe("Server should", () => {
         it("and return all toys in durham", (done) => {
             request.get(base_url + 'toys/all/durham', (error, response, body) => {
                 expect(body).toBeTruthy();
-                expect(body).toContain("");
+                expect(body).toContain(JSON.stringify(mockResponseToysDurham));
                 done();
             });
         });
@@ -41,15 +49,15 @@ describe("Server should", () => {
         it("and return all bikes in raleigh", (done) => {
             request.get(base_url + 'bike/all/raleigh', (error, response, body) => {
                 expect(body).toBeTruthy();
-                expect(body).toContain("");
+                expect(body).toContain(JSON.stringify(mockResponseBikeRaleigh));
                 done();
             });
         });
 
-        it("and return all fgoods in raleigh", (done) => {
+        it("and return all foods in raleigh", (done) => {
             request.get(base_url + 'food/all/raleigh', (error, response, body) => {
                 expect(body).toBeTruthy();
-                expect(body).toContain("");
+                expect(body).toContain(JSON.stringify(mockResponseFoodRaleigh));
                 done();
             });
         });
@@ -57,7 +65,7 @@ describe("Server should", () => {
         it("and return all toys in raleigh", (done) => {
             request.get(base_url + 'toys/all/raleigh', (error, response, body) => {
                 expect(body).toBeTruthy();
-                expect(body).toContain("");
+                expect(body).toContain(JSON.stringify(mockResponseToysRaleigh));
                 done();
             });
         });
@@ -77,14 +85,14 @@ describe("Server should", () => {
         });
     });
 
-    describe("GET /servicename/team", () => {
-        it("and return <b>My</b> first express http server", (done) => {
-            request.get(base_url, (error, response, body) => {
-                expect(body).toBeTruthy();
-                expect(body).toContain("<b>My</b> first express http server");
-                done();
-            });
-        });
-    });
+    // describe("GET /servicename/team", () => {
+    //     it("and return <b>My</b> first express http server", (done) => {
+    //         request.get(base_url, (error, response, body) => {
+    //             expect(body).toBeTruthy();
+    //             expect(body).toContain("<b>My</b> first express http server");
+    //             done();
+    //         });
+    //     });
+    // });
 
 });
