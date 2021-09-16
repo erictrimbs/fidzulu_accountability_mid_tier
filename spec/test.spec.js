25ffdd278454a4ea94b6f6b94788b8ac0a990900
Dennisn1010 <90853201+Dennisn1010@users.noreply.github.com>
2021-09-16 14:27:52 -0400

    basic tests in place

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -12,4 +12,79 @@ describe("Server should", () => {
             });
         });
     });
+
+    describe("GET /:servicename/all/:location", () => {
+        it("and return all bikes in durham", (done) => {
+            request.get(base_url + 'bike/all/durham', (error, response, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain("");
+                done();
+            });
+        });
+
+        it("and return all foods in durham", (done) => {
+            request.get(base_url + 'food/all/durham', (error, response, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain("");
+                done();
+            });
+        });
+
+        it("and return all toys in durham", (done) => {
+            request.get(base_url + 'toys/all/durham', (error, response, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain("");
+                done();
+            });
+        });
+
+        it("and return all bikes in raleigh", (done) => {
+            request.get(base_url + 'bike/all/raleigh', (error, response, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain("");
+                done();
+            });
+        });
+
+        it("and return all fgoods in raleigh", (done) => {
+            request.get(base_url + 'food/all/raleigh', (error, response, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain("");
+                done();
+            });
+        });
+
+        it("and return all toys in raleigh", (done) => {
+            request.get(base_url + 'toys/all/raleigh', (error, response, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain("");
+                done();
+            });
+        });
+
+        it("and returns 404 for invalid service", (done) => {
+            request.get(base_url + 'apartments/all/raleigh', (error, response, body) => {
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+        });
+        
+        it("and returns 404 for invalid city", (done) => {
+            request.get(base_url + 'toys/all/ashville', (error, response, body) => {
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+        });
+    });
+
+    describe("GET /servicename/team", () => {
+        it("and return <b>My</b> first express http server", (done) => {
+            request.get(base_url, (error, response, body) => {
+                expect(body).toBeTruthy();
+                expect(body).toContain("<b>My</b> first express http server");
+                done();
+            });
+        });
+    });
+
 });
