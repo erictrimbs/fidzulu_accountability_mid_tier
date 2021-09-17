22612224de94e1b5d8f5c68a5ea52517584805be
a666084 <ryan.miller@fmr.com>
2021-09-17 14:46:36 -0400

    Fixed tests for books, dvds, laptops

--- GroupB/spec/books_spec.js
+++ GroupB/spec/books_spec.js
@@ -1,23 +1,52 @@
 const request = require("request");
-const data = require('../data/booksteam.json');
 const url = 'http://localhost:3022';
 
 describe("Books Test", function () {
     
-    describe("GET classb/books", () => {
+    describe("GET classb/books/all/raleigh", () => {
         it ("returns status code 200", (done) => {
-            request.get(url + '/classb/books', (error, response, body) => {
+            request.get(url + '/classb/books/all/raleigh', (error, response, body) => {
                 expect(response.statusCode).toBe(200);
+                expect(response.body).toContain("Title")
+                expect(response.body).toContain("Author")
+                expect(response.body).toContain("price")
+                expect(response.body).toContain("ISBN")
+                expect(response.body).toContain("publisher")
                 done();
             });
         });
     });
 
     //test for wrong path and expect 404
-    describe("GET classb/boooks", () => {
+    describe("GET classb/boooks/all/raleigh", () => {
         // accessing wrong path
         it("returns status code 404",  (done) => {
-            request.get(url + '/classb/boook', (error, response, body) => {
+            request.get(url + '/classb/boook/all/raleigh', (error, response, body) => {
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+        });
+    });
+
+    describe("GET classb/books/all/durham", () => {
+        it ("returns status code 200", (done) => {
+            request.get(url + '/classb/books/all/durham', (error, response, body) => {
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toContain("Title")
+                expect(response.body).toContain("Author")
+                expect(response.body).toContain("price")
+                expect(response.body).toContain("ISBN")
+                expect(response.body).toContain("publisher")
+                done();
+            });
+        });
+    });
+
+    //test for wrong path and expect 404
+    describe("GET classb/boooks/all/durham", () => {
+        // accessing wrong path
+        it("returns status code 404",  (done) => {
+            request.get(url + '/classb/boook/all/durham', (error, response, body) => {
                 expect(response.statusCode).toBe(404);
                 done();
             });
@@ -28,7 +57,7 @@ describe("Books Test", function () {
         it ("returns status code 200", (done) => {
             request.get(url + '/classb/books/team', (error, response, body) => {
                 expect(response.statusCode).toBe(200);
-                expect(response.body).toBe(data);
+                expect(response.body).toContain("Team 1");
                 done();
             });
         });
--- GroupB/spec/dvds_spec.js
+++ GroupB/spec/dvds_spec.js
@@ -1,23 +1,50 @@
 const request = require("request");
-const data = require('../data/dvdsteam.json');
 const url = 'http://localhost:3022';
 
 describe("DVDS Test", function () {
     
-    describe("GET classb/dvds", () => {
+    describe("GET classb/dvds/all/raleigh", () => {
         it ("returns status code 200", (done) => {
-            request.get(url + '/classb/dvds', (error, response, body) => {
+            request.get(url + '/classb/dvds/all/raleigh', (error, response, body) => {
                 expect(response.statusCode).toBe(200);
+                expect(response.body).toContain("title");
+                expect(response.body).toContain("mpaa_rating");
+                expect(response.body).toContain("studio");
+                expect(response.body).toContain("price");
                 done();
             });
         });
     });
 
     //test for wrong path and expect 404
-    describe("GET classb/dvvds", () => {
+    describe("GET classb/dvvds/all/raleigh", () => {
         // accessing wrong path
         it("returns status code 404",  (done) => {
-            request.get(url + '/classb/dvvds', (error, response, body) => {
+            request.get(url + '/classb/dvvds/all/raleigh', (error, response, body) => {
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+        });
+    });
+
+    describe("GET classb/dvds/all/durham", () => {
+        it ("returns status code 200", (done) => {
+            request.get(url + '/classb/dvds/all/durham', (error, response, body) => {
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toContain("title");
+                expect(response.body).toContain("mpaa_rating");
+                expect(response.body).toContain("studio");
+                expect(response.body).toContain("price");
+                done();
+            });
+        });
+    });
+
+    //test for wrong path and expect 404
+    describe("GET classb/dvvds/all/durham", () => {
+        // accessing wrong path
+        it("returns status code 404",  (done) => {
+            request.get(url + '/classb/dvvds/all/durham', (error, response, body) => {
                 expect(response.statusCode).toBe(404);
                 done();
             });
@@ -28,7 +55,7 @@ describe("DVDS Test", function () {
         it ("returns status code 200", (done) => {
             request.get(url + '/classb/dvds/team', (error, response, body) => {
                 expect(response.statusCode).toBe(200);
-                expect(response.body).toBe(data);
+                expect(response.body).toContain("Team 3");
                 done();
             });
         });
