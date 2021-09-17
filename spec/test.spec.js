64a4365f5748d9e088cb911afd39357c7b1f3cee
Dennisn1010 <90853201+Dennisn1010@users.noreply.github.com>
2021-09-17 15:37:10 -0400

    test updates

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -2,18 +2,184 @@ const request = require("request");
 
 const base_url = 'http://localhost:3021/classA/';
 
-// TODO: change to proper response format
-const mockResponseBikesDurham = { 'request is': 'valid', 'service name': 'bike', 'location': 'durham' };
-const mockResponseFoodDurham = { 'request is': 'valid', 'service name': 'food', 'location': 'durham' };
-const mockResponseToysDurham = { 'request is': 'valid', 'service name': 'toys', 'location': 'durham' };
+const mockResponseBikesDurham = [
+    {
+        "name": "Mamba Sport 12\" Balance Bike",
+        "brand": "Mamba Bikes",
+        "color": "black",
+        "price": 75.88 * 0.08
+    },
+    {
+        "name": "DJ Fat Bike 500W",
+        "brand": "DJ Bikes",
+        "color": "grey",
+        "price": 1599.86 * 0.08
+    },
+    {
+        "name": "Kobe Aluminum Balance",
+        "brand": "Kobe",
+        "color": "blue",
+        "price": 88.56 * 0.08
+    },
+    {
+        "name": "Pomona Men's Cruiser Bike",
+        "brand": "Northwoods",
+        "color": "silver",
+        "price": 221.36 * 0.08
+    }
+];
+const mockResponseFoodDurham = [
+    {
+        "name": "The Original Sandwich",
+        "brand": "Oreo",
+        "weight": "303g",
+        "calories": 405,
+        "price": 2.85 * 0.08
+    },
+    {
+        "name": "Peanut Butter",
+        "brand": "KRAFT",
+        "weight": "2000g",
+        "calories": 726,
+        "price": 9.39 * 0.08
+    },
+    {
+        "name": "Beef Ravioli",
+        "brand": "Chef Boyardee",
+        "weight": "425g",
+        "calories": 250,
+        "price": 2.45 * 0.08
+    },
+    {
+        "name": "Medium Cheddar Cheese",
+        "brand": "MOON CHEESE",
+        "weight": "57g",
+        "calories": 525,
+        "price": 5.87 * 0.08
+    }
+];
+const mockResponseToysDurham = [
+    {
+        "name": "Medical Kit",
+        "brand": "Fisher-Price",
+        "age-group": "3 to 9",
+        "prize": 20.41 * 0.08
+    },
+    {
+        "name": "Ferry Boat",
+        "brand": "Green Toys",
+        "age-group": "3 to 6",
+        "prize": 13.26 * 0.08
+    },
+    {
+        "name": "Rock-a-Stack",
+        "brand": "Fisher-Price",
+        "age-group": "1 to 5",
+        "prize": 5.99 * 0.08
+    },
+    {
+        "name": "Stack Up Cups",
+        "brand": "The First Years",
+        "age-group": "0 to 3",
+        "prize": 3.99 * 0.08
+    }
+];
 
-const mockResponseBikesRaleigh = { 'request is': 'valid', 'service name': 'bike', 'location': 'raleigh' };
-const mockResponseFoodRaleigh = { 'request is': 'valid', 'service name': 'food', 'location': 'raleigh' };
-const mockResponseToysRaleigh = { 'request is': 'valid', 'service name': 'toys', 'location': 'raleigh' };
+const mockResponseBikesRaleigh = [
+    {
+        "name": "Mamba Sport 12\" Balance Bike",
+        "brand": "Mamba Bikes",
+        "color": "black",
+        "price": 75.88 * 0.075
+    },
+    {
+        "name": "DJ Fat Bike 500W",
+        "brand": "DJ Bikes",
+        "color": "grey",
+        "price": 1599.86 * 0.075
+    },
+    {
+        "name": "Kobe Aluminum Balance",
+        "brand": "Kobe",
+        "color": "blue",
+        "price": 88.56 * 0.075
+    },
+    {
+        "name": "Pomona Men's Cruiser Bike",
+        "brand": "Northwoods",
+        "color": "silver",
+        "price": 221.36 * 0.075
+    }
+];
+const mockResponseFoodRaleigh = [
+    {
+        "name": "The Original Sandwich",
+        "brand": "Oreo",
+        "weight": "303g",
+        "calories": 405,
+        "price": 2.85 * 0.075
+    },
+    {
+        "name": "Peanut Butter",
+        "brand": "KRAFT",
+        "weight": "2000g",
+        "calories": 726,
+        "price": 9.39 * 0.075
+    },
+    {
+        "name": "Beef Ravioli",
+        "brand": "Chef Boyardee",
+        "weight": "425g",
+        "calories": 250,
+        "price": 2.45 * 0.075
+    },
+    {
+        "name": "Medium Cheddar Cheese",
+        "brand": "MOON CHEESE",
+        "weight": "57g",
+        "calories": 525,
+        "price": 5.87 * 0.075
+    }
+];
+const mockResponseToysRaleigh = [
+    {
+        "name": "Medical Kit",
+        "brand": "Fisher-Price",
+        "age-group": "3 to 9",
+        "prize": 20.41 * 0.075
+    },
+    {
+        "name": "Ferry Boat",
+        "brand": "Green Toys",
+        "age-group": "3 to 6",
+        "prize": 13.26 * 0.075
+    },
+    {
+        "name": "Rock-a-Stack",
+        "brand": "Fisher-Price",
+        "age-group": "1 to 5",
+        "prize": 5.99 * 0.075
+    },
+    {
+        "name": "Stack Up Cups",
+        "brand": "The First Years",
+        "age-group": "0 to 3",
+        "prize": 3.99 * 0.075
+    }
+];;
 
-const teamMembersBikes = { 'team': 'bikes', 'membersName': ['Aidan Lee', 'Byron Martinez', 'Connor Renquin', 'Noor Helbaoui'] }
-const teamMembersFood = { 'team': 'food', 'membersName': ['Tiffany Ngo', 'Rhea Bhatia', 'Marissa Greise', 'Gary Chen'] }
-const teamMembersToys = { 'team': 'toys', 'membersName': ['Alex Eseyin', 'Anjana Rajmani', 'Jacob DiSpirito', 'Melissa Parkinson'] }
+const teamMembersBikes = {
+    "team": "Team 1",
+    "membersNames": ["Aidan Lee", "Byron Martinez", "Connor Renquin", "Noor Helbaoui"]
+}
+const teamMembersFood = {
+    "team": "Team 3",
+    "membersNames": ["Rhea Bhatia", "Tiffany Ngo", "Gary Chen", "Marissa Greise"]
+};
+const teamMembersToys = {
+    "team": "Team 5",
+    "memberNames": ["Melissa Parkinson", "Alex Eseyin", "Jacob DiSpirito", "Anjana Rajamani"]
+};
 
 describe("Server should", () => {
     describe("GET /:servicename/all/:location", () => {
@@ -71,7 +237,7 @@ describe("Server should", () => {
                 done();
             });
         });
-        
+
         it("and returns 404 for invalid city", (done) => {
             request.get(base_url + 'toys/all/ashville', (error, response, body) => {
                 expect(response.statusCode).toBe(404);
